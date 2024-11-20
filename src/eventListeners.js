e0c5039e53b1e35588de3812a881f215bab2403e
moomins07 <davidrsmith04@gmail.com>
2024-11-20 00:27:41 +0000

    carousel working - adding animations

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,9 +1,17 @@
 import { displayDropDownMenu } from "./dropDownMenu"
+import { carouselRight } from "./carousel"
+import { carouselLeft } from "./carousel"
 
 function setEventListeners() {
     document.querySelectorAll('.dropdown-btn').forEach((btn) => {
         btn.addEventListener('click', displayDropDownMenu)
     })
+
+
+    document.querySelector('.carousel-button.right').addEventListener('click', carouselRight)
+    document.querySelector('.carousel-button.left').addEventListener('click', carouselLeft)
+
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles/main.scss';
 import { setEventListeners } from './eventListeners';
+import { updateImage } from "./carousel"
+
 
 document.addEventListener('DOMContentLoaded', () => {
     setEventListeners();
+    updateImage()
 
 });
